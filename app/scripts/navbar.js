fe787357fd7647b5ac3848aca25b961301910675
Utomo Saputra <utomo.saputra@compare88.net>
2017-08-11 17:06:00 +0700

    Add script to display navigation menu
    When clicked on hamburger-btn it will display a fixed full height menu on screen
    
    * add function on hamburger-btn

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -2,12 +2,15 @@
  * File navbar.js
  *
  * Handles auto-hide navbar when scroll down and up pages
+ * Handles auto-display hamburger menu
  */
 
 ( function() {
 
     var lastScrollTop = 0;
     var navbar        = $('.navbar-fixed-top');
+    var navigation    = $('.navigation.overlay');
+    var body          = $('body');
 
     $(window).scroll(function(event){
 
@@ -22,4 +25,20 @@
 
         lastScrollTop = st;
     });
+
+    // auto display hamburger menu
+
+    $(".hamburger-btn").click(function(){
+
+        // change hamburger menu to close button
+        $(this).toggleClass("is-active");
+        
+        // check body has class modal open\
+        body.toggleClass('modal-open-clear');
+
+        // toggle overlay navigation
+        navigation.fadeToggle(200);
+
+    });
+
 } )();
